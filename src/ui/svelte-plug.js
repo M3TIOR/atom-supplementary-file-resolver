70bba8b0257729aa3d2b0e35f9629d74717dbb31
Ruby Allison Rose <M3TIOR@users.noreply.github.com>
2020-05-01 20:25:37 -0700

    fix(svelte): Removed syntax issue breaking the Plug Component Constructor

--- src/ui/svelte-plug.js
+++ src/ui/svelte-plug.js
@@ -47,7 +47,7 @@ export default class SveltePlug {
 		this.element = document.createElement('div');
 
 		componentOptions = Object.assign(componentOptions || {}, {target: this.element});
-		this.svelte = new ComponentConstructor(...componentOptions);
+		this.svelte = new ComponentConstructor(componentOptions);
 	}
 
 	// Tear down any state and detach
